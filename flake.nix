38919c500fe45ea92ab9a4c825f0976ed3d0f093
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2023-12-13 17:03:18 +0100

    reformat

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
-inputs = {
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
     {
       devShells.${system}.default = pkgs.mkShellNoCC {
         shellHook = "echo hello";
