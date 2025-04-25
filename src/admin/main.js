b9017a653b06f55be4241431fda3bb8d0a0bc642
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2025-04-25 15:29:12 +0000

    Add initial HTML, CSS, and JavaScript file for admin interface

---
+++ src/admin/main.js
@@ -0,0 +1,11 @@
+/*
+fetch("/api/lastImageIndex", {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ lastImageIndex: imageIndex }),
+    }).catch((e) => {
+        console.warn(e)
+    })
+*/
